0eac3275def39ce93f85039fe5c7b2a5c81faa58
yipqp <rdyip892@gmail.com>
2023-02-20 19:25:45 -0800

    Add event listener to squares

--- src/script.js
+++ src/script.js
@@ -4,16 +4,34 @@ const Player = (symbol) => {
 };
 
 const Gameboard = (function () {
-  let board = [];
+  const board = [];
+  const player1 = Player("x");
+  const player2 = Player("o");
+  let currentPlayer = player1;
 
-  for (let row = 0; row < 3; row++) {
-    board[row] = [];
-    for (let col = 0; col < 3; col++) {
-      board[row][col] = 0;
+  const resetBoard = () => {
+    for (let row = 0; row < 3; row++) {
+      board[row] = [];
+      for (let col = 0; col < 3; col++) {
+        board[row][col] = 0;
+      }
     }
-  }
+  };
+  resetBoard();
 
   const getBoard = () => board;
+
+  const symbolToPlayer = (symbol) => {
+    let winner;
+    if (symbol === Gameboard.getPlayer1().getSymbol()) {
+      winner = Gameboard.getPlayer1();
+    } else if (symbol === Gameboard.getPlayer2().getSymbol()) {
+      winner = Gameboard.getPlayer2();
+    } else {
+      winner = undefined;
+    }
+    return winner;
+  };
 
   const checkWin = () => {
     // row win
@@ -26,7 +44,7 @@ const Gameboard = (function () {
           break;
         }
       }
-      if (tempSymbol === winningSymbol) return winningSymbol;
+      if (tempSymbol === winningSymbol) return symbolToPlayer(winningSymbol);
     }
 
     // col win
@@ -39,7 +57,7 @@ const Gameboard = (function () {
           break;
         }
       }
-      if (tempSymbol === winningSymbol) return winningSymbol;
+      if (tempSymbol === winningSymbol) return symbolToPlayer(winningSymbol);
     }
     // diag left-right win
     for (let i = 0; i < 1; i++) {
@@ -51,7 +69,7 @@ const Gameboard = (function () {
           break;
         }
       }
-      if (tempSymbol === winningSymbol) return winningSymbol;
+      if (tempSymbol === winningSymbol) return symbolToPlayer(winningSymbol);
     }
     // diag right-left win
     for (let i = 0; i < 1; i++) {
@@ -63,9 +81,18 @@ const Gameboard = (function () {
           break;
         }
       }
-      if (tempSymbol === winningSymbol) return winningSymbol;
+      if (tempSymbol === winningSymbol) return symbolToPlayer(winningSymbol);
     }
     return false;
+  };
+
+  const checkDraw = () => {
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
+        if (board[r][c] === 0) return false;
+      }
+    }
+    return true;
   };
 
   const makeMove = (symbol, row, col) => {
@@ -74,23 +101,69 @@ const Gameboard = (function () {
     return true;
   };
 
-  return { getBoard, checkWin, makeMove };
+  const getTurn = () => currentPlayer.getSymbol();
+
+  const switchTurn = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const getPlayer1 = () => player1;
+  const getPlayer2 = () => player2;
+
+  return {
+    getBoard,
+    checkWin,
+    checkDraw,
+    makeMove,
+    getTurn,
+    switchTurn,
+    getPlayer1,
+    getPlayer2,
+    resetBoard,
+  };
 })();
 
 const DisplayController = (function () {
-  const updateBoard = () => {
+  const updateDisplay = () => {
     const board = Gameboard.getBoard();
     for (let r = 0; r < board.length; r++) {
       for (let c = 0; c < board[r].length; c++) {
-        if (board[r][c] !== 0) {
-          const boardSquare = document.querySelector(
-            `[data-row="${r}"][data-col="${c}"]`
-          );
-          boardSquare.textContent = board[r][c];
-        }
+        const boardSquare = document.querySelector(
+          `[data-row="${r}"][data-col="${c}"]`
+        );
+
+        boardSquare.textContent = board[r][c] !== 0 ? board[r][c] : " ";
       }
     }
   };
 
-  return { updateBoard };
+  const squares = document.querySelectorAll(".gameboard-square");
+  squares.forEach((square) => {
+    square.addEventListener("click", (e) => {
+      const {
+        dataset: { row, col },
+      } = e.target;
+
+      if (!Gameboard.makeMove(Gameboard.getTurn(), row, col)) return;
+
+      updateDisplay();
+
+      const winner = Gameboard.checkWin();
+
+      if (winner) {
+        // do something
+        console.log(`${winner.getSymbol()} won!`);
+        Gameboard.resetBoard();
+        updateDisplay();
+      } else if (Gameboard.checkDraw()) {
+        console.log(`No one won.`);
+        Gameboard.resetBoard();
+        updateDisplay();
+      }
+
+      Gameboard.switchTurn();
+    });
+  });
+
+  return { updateDisplay };
 })();
